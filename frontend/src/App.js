7516d337645c3201b98ce5f44703c4906ec927bd
gichukipitar <ptahgichuki@yahoo.com>
2023-03-02 16:09:01 +0300

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 
 function App() {
-  return (
-      <div>
-          <HeaderComponent/>
-          <div className="container">
-              <ListEmployeeComponent/>
-          </div>
-          <FooterComponent/>
-      </div>
+    return (
+        <div>
+            <Router>
+                <div >
+                    <HeaderComponent/>
+                    <div className="container">
+                        <Routes>
+                            <Route path='/' element={<ListEmployeeComponent/>}/>
+                            <Route path='/employees' element={<ListEmployeeComponent/>}/>
 
-  );
+                        </Routes>
+
+                    </div>
+                    <FooterComponent/>
+                </div>
+            </Router>
+        </div>
+
+    );
 }
 
 export default App;
