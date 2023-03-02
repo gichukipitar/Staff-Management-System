ed7a8748c76c894b61a21420d9fa2b931a5020c3
gichukipitar <ptahgichuki@yahoo.com>
2023-03-02 14:59:27 +0300

    new front end files

---
+++ frontend/src/Services/EmployeeService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+const EMPLOYEE_BASE_URL = "http://localhost:8080/employeemanagamentsystem/employee/listall"
+class EmployeeService{
+    getEmployees(){
+        return axios.get(EMPLOYEE_BASE_URL);
+    }
+
+
+}
+export default new EmployeeService()
